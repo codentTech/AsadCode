747ab630969035e959eac421b0f4523e91e9f071
Asad <theasadabbasdev@gmail.com>
2025-06-04 18:06:59 +0300

    modifications

--- src/components/campaign/campaigns/components/applications/components/creator-spend-analysis/creator-spend-analysis.component.jsx
+++ src/components/campaign/campaigns/components/applications/components/creator-spend-analysis/creator-spend-analysis.component.jsx
@@ -167,7 +167,7 @@ const CreatorSpendAnalysis = () => {
             <p className="text-sm text-gray-600">Similar niches and high engagement rates</p>
           </div>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
             {creators.map((creator) => (
               <div
                 key={`rec-${creator.id}`}
