036eec717548297d245ff5d90012cf443aa2b437
codentTech <theasadabbasdev@gmail.com>
2025-08-01 20:51:28 +0500

    build error resolved

--- src/app/settings/page.jsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import Auth from "@/auth/auth.component";
-import AUTH from "@/common/constants/auth.constant";
-import AccountSettings from "@/components/settings/account-settings/account-settings.component";
-
-export default function Page() {
-  return <Auth component={<AccountSettings />} type={AUTH.PRIVATE} />;
-}
