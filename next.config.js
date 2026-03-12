1e767a4c867c968e983b8232d1edd082d211b0dd
Asad <theasadabbasdev@gmail.com>
2026-03-12 11:27:19 +0300

    sentry setup

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
+import { withSentryConfig } from "@sentry/nextjs";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+const sentryEnabled =
+  process.env.SENTRY_ENABLED === "true" && process.env.SENTRY_DSN;
+
+export default sentryEnabled
+  ? withSentryConfig(nextConfig, {
+      silent: true,
+      org: process.env.SENTRY_ORG,
+      project: process.env.SENTRY_PROJECT,
+    })
+  : nextConfig;
